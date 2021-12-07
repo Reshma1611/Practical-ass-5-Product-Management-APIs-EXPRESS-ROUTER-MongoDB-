32138b094cba458ffce62dd784b2cbbaa21ed9fc
Reshma1611 <81482031+Reshma1611@users.noreply.github.com>
2021-12-07 17:30:44 +0530

    updateToFetchDetail

--- app.js
+++ app.js
@@ -16,12 +16,60 @@ app.use("/company", comapnyRouter);
 
 //product
 const productRouter = require("./routes/productRoute"); 
-app.use("/company", productRouter); 
+app.use("/product", productRouter); 
 
 //seller
 const sellerRouter = require("./routes/sellerRoute"); 
+//const productModel = require("./schemas/productSchema");
 app.use("/seller", sellerRouter); 
+//
+const companyModel = require("./schemas/companySchema"); 
+const productModel = require("./schemas/productSchema"); 
+const sellerModel = require("./schemas/sellerSchema"); 
 
+
+//fetch company details based on product name
+app.post("/fetchCompanyDetailBasedProductName", async (req, res) => {
+    
+    const productModel = require("./schemas/productSchema"); 
+    const name = req.body.productName;
+    const allProduct = await productModel.find();
+    var a=0;
+    
+
+    const allCompany = await productModel.find();
+    var pid;
+    for(var i = 0; i< allProduct.length; i++)
+    {
+       
+        //res.json({data: allProduct[i].title});
+        if(allProduct[i].title === name )
+        {
+            pid = allProduct[i].productId;
+        }
+        
+    }
+    
+    return res.json(pid);
+
+    
+//    return res.json({data: allProduct.length});
+//    const productName = productModel.filter((p) => (p.title === name));
+/*
+    if(productName.length === 1)
+    {
+        const companyDetail = companyModel.filter((c) => 
+            c.productIds.forEach(pid => {
+                if(pid === p.productIds) {
+                    res.json({data: pid});
+                }
+            })
+        
+        
+        );
+    }
+*/
+});
 
 
 
